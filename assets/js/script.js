1e39d817c4eed2622e23f897a1a019517007b0d3
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-04-03 00:44:19 +0700

    con moi con ran hoi xau

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,16 +15,70 @@ let food = {
 const dead = new Audio();
 const eat = new Audio();
 const level_up = new Audio();
+const theme = new Audio();
 
 dead.src = "assets/audio/dead.mp3";
 eat.src = "assets/audio/eat.mp3";
 level_up.src = "assets/audio/level_up.mp3";
+theme.src = "assets/audio/snake_theme.mp3";
+
+theme.loop = true;
+
+
 let score = 0;
 let snake = [];
 snake[0] = {
     x : 9 * unit_snake,
     y : 10 * unit_snake
 }
+
+var isDead = false;
+// buttons 
+const backgroundReset = new Image();
+backgroundReset.src = "assets/images/snake_reset.jpg";
+
+const buttonReset = {
+    x : 5*unit_snake,
+    y : 11*unit_snake,
+    width: 7*unit_snake,
+    height: 2*unit_snake,
+    text: "play again!"
+};
+
+const buttonTheme = {
+    x: 13*unit_snake,
+    y: 11*unit_snake,
+    width: 2*unit_snake,
+    height: 2*unit_snake,
+    text: "🎵",
+    textPlay: "🎵",
+    textPaused: "⏸️"
+}
+
+function drawButton() {
+    ctx_snake.fillStyle = "#3498db"; 
+    ctx_snake.fillRect(buttonReset.x, buttonReset.y, buttonReset.width, buttonReset.height);
+    
+    ctx_snake.fillStyle = "#ffffff"; 
+    ctx_snake.font = "16px Arial";
+    ctx_snake.textAlign = "center";
+    ctx_snake.textBaseline = "middle";
+    ctx_snake.fillText(buttonReset.text, buttonReset.x + buttonReset.width / 2, buttonReset.y + buttonReset.height / 2);
+}
+
+function drawButtonMusic() {
+    ctx_snake.fillStyle = "#3498db"; 
+    ctx_snake.fillRect(buttonTheme.x, buttonTheme.y, buttonTheme.width, buttonTheme.height);
+    
+    ctx_snake.fillStyle = "#ffffff"; 
+    ctx_snake.font = "16px Arial";
+    ctx_snake.textAlign = "center";
+    ctx_snake.textBaseline = "middle";
+    ctx_snake.fillText(buttonTheme.text, buttonTheme.x + buttonTheme.width / 2, buttonTheme.y + buttonTheme.height / 2);
+}
+// end buttons 
+
+
 
 // control 
 
@@ -150,9 +204,16 @@ function draw(){
     };
     // game over 
     // console.log(d);
+    
     if(snakeHeadX < unit_snake || snakeHeadX > 18 * unit_snake || snakeHeadY < 4*unit_snake || snakeHeadY > 18 * unit_snake || collision(newHead, snake)){
         clearInterval(game_snake);
         dead.play();
+        isDead = true;
+        setTimeout(() => {
+            ctx_snake.drawImage(backgroundReset, 5*unit_snake, 3*unit_snake, 10*unit_snake, 7*unit_snake);
+            drawButton();
+            drawButtonMusic();
+        }, 3000);
         // console.log(newHead);
         // console.log(snake);
         
@@ -170,6 +231,51 @@ function draw(){
     queuedDirection = null;
 }
 // end draw 
+
+// click event 
+cvs_snake.addEventListener("click", function(event) {
+    const rect = cvs_snake.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    if(isDead) {
+        if (mouseX >= buttonReset.x && mouseX <= buttonReset.x + buttonReset.width &&
+            mouseY >= buttonReset.y && mouseY <= buttonReset.y + buttonReset.height) {
+            score = 0;
+            snake = [];
+            snake[0] = {
+                x : 9 * unit_snake,
+                y : 10 * unit_snake
+            }
+            food = {
+                x : Math.floor(Math.random() * 18 + 1) * unit_snake,
+                y : Math.floor(Math.random() * 15 + 4) * unit_snake
+            };
+            d = ""
+            queuedDirection = null;
+            isDead = false;
+            game_snake = setInterval(draw, 100);  
+        }
+    
+        if (mouseX >= buttonTheme.x && mouseX <= buttonTheme.x + buttonTheme.width &&
+            mouseY >= buttonTheme.y && mouseY <= buttonTheme.y + buttonTheme.height) {
+            if(theme.paused) {
+                theme.play();
+                buttonTheme.text = buttonTheme.textPaused;
+                drawButtonMusic();
+            } else {
+                theme.pause();
+                buttonTheme.text = buttonTheme.textPlay;
+                drawButtonMusic();
+            }
+        }
+    }
+    
+});
+
+// end click event 
+
+
 // render
 let game_snake =  setInterval(draw, 100);
 // end render 
