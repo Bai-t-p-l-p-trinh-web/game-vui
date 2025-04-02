4e79bfb9fb9e9756f48cbd49e77ae44dd015af66
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-04-03 01:00:54 +0700

    fix bugs

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const sidebar = document.createElement("div");
   sidebar.classList.add("sidebar");
   sidebar.innerHTML = `
-        <button onclick="navigateTo('index.html')">🏠 Home</button>
-        <button onclick="navigateTo('games/game1.html')">🎮 Game 1</button>
-        <button onclick="navigateTo('games/game2.html')">🎮 Game 2</button>
-        <button onclick="navigateTo('games/game3.html')">🎮 Game 3</button>
-        <button onclick="navigateTo('games/game4.html')">🎮 Game 4</button>
+        <button onclick="navigateTo('/game-vui/index.html')">🏠 Home</button>
+        <button onclick="navigateTo('/game-vui/games/game1.html')">🎮 Game 1</button>
+        <button onclick="navigateTo('/game-vui/games/game2.html')">🎮 Game 2</button>
+        <button onclick="navigateTo('/game-vui/games/game3.html')">🎮 Game 3</button>
+        <button onclick="navigateTo('/game-vui/games/game4.html')">🎮 Game 4</button>
     `;
   document.body.insertAdjacentElement("afterbegin", sidebar);
 });
