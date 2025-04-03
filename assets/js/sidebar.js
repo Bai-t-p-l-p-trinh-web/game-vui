937d24d69d7d8eeb754f83f60238583f45c92810
Tien Lam <nhattienlam2005@gmail.com>
2025-04-03 15:59:32 +0700

    update homepage, sidebar UI, modify game3's music logic

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -2,11 +2,37 @@ document.addEventListener("DOMContentLoaded", function () {
   const sidebar = document.createElement("div");
   sidebar.classList.add("sidebar");
   sidebar.innerHTML = `
-        <button onclick="navigateTo('/game-vui/index.html')">🏠 Home</button>
-        <button onclick="navigateTo('/game-vui/games/game1.html')">🎮 Game 1</button>
-        <button onclick="navigateTo('/game-vui/games/game2.html')">🎮 Game 2</button>
-        <button onclick="navigateTo('/game-vui/games/game3.html')">🎮 Game 3</button>
-        <button onclick="navigateTo('/game-vui/games/game4.html')">🎮 Game 4</button>
+        <button onclick="navigateTo('index.html')">🏠 Home</button>
+        <button onclick="navigateTo('game1.html')">🎮 Game 1</button>
+        <button onclick="navigateTo('game2.html')">🎮 Game 2</button>
+        <button onclick="navigateTo('game3.html')">🎮 Game 3</button>
+        <button onclick="navigateTo('game4.html')">🎮 Game 4</button>
+        <button id="toggle-sound">🔊 Tắt âm thanh</button>
     `;
   document.body.insertAdjacentElement("afterbegin", sidebar);
+
+  let isSoundOn = true;
+
+  const toggleSoundButton = document.getElementById("toggle-sound");
+  toggleSoundButton.addEventListener("click", function () {
+    isSoundOn = !isSoundOn;
+
+    toggleSoundButton.textContent = isSoundOn
+      ? "🔊 Tắt âm thanh"
+      : "🔇 Mở âm thanh";
+
+    const audios = document.querySelectorAll("audio");
+    audios.forEach((audio) => {
+      if (isSoundOn) {
+        audio.muted = false;
+      } else {
+        audio.muted = true;
+      }
+    });
+
+    const toggleSoundEvent = new CustomEvent("toggleSound", {
+      detail: { isSoundOn },
+    });
+    document.dispatchEvent(toggleSoundEvent);
+  });
 });
